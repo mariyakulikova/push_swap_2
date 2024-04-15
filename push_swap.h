51cd42d68cc0bc5e390fc31f526f92be8975eef3
mkulikov <mkulikov@student.42berlin.de>
2024-04-15 11:13:07 +0200

    refactor header file

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 14:34:50 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/04/14 21:45:04 by mkulikov         ###   ########.fr       */
+/*   Updated: 2024/04/15 11:11:53 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,36 +42,47 @@ typedef struct s_ps
 	t_stack	*b;
 }			t_ps;
 
-t_dlst	*dlst_new(int value);
-int		split_size(char **s);
-void	free_split(char **s);
-long	alpha_to_long(char *nptr);
-int		is_stack_sorted(t_dlst *head);
-int		stack_size(t_dlst *head);
-void	free_dlst_list(t_dlst *head);
-t_stack	*stack_init();
-void	set_stack_max_min(t_stack *stack);
-int		free_and_exit(t_ps *params, int exit_status);
-t_dlst	*get_stack(int *tab, int size);
-void	sort(t_ps *params);
-
-/* cmd_utils.c */
-void	swap_first_2_elem(t_stack *stack);
-void	put_at_top(t_stack *stack1, t_stack *stack2);
-
-/* cmd.c */
-void	sa(t_stack *a);
-void	sb(t_stack *b);
-void	ss(t_stack *a, t_stack *b);
+/* cmd_put.c */
 void	pa(t_stack *a, t_stack *b);
 void	pb(t_stack *b, t_stack *a);
 
-/* stack2.c */
-void	free_stack(t_stack *stack);
+/* cmd_rotate */
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+
+/* cmd_swap.c */
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+
+/* dlst.c */
+t_dlst	*dlst_new(int value);
+void	free_dlst_list(t_dlst *head);
+t_dlst	*dlstlast(t_dlst *head);
 
 /* params.c */
 void	set_stack_params(t_ps *params, int *tab, int size);
 t_ps	*params_init();
+
+/* sort.c */
+void	sort(t_ps *params);
+
+/* stack.c */
+int		is_stack_sorted(t_dlst *head);
+int		stack_size(t_dlst *head);
+t_stack	*stack_init();
+void	set_stack_max_min(t_stack *stack);
+t_dlst	*get_stack(int *tab, int size);
+
+/* stack2.c */
+void	free_stack(t_stack *stack);
+
+/* utils.c */
+void	free_split(char **s);
+int		split_size(char **s);
+int		free_and_exit(t_ps *params, int exit_status);
+long	alpha_to_long(char *nptr);
 
 /* test */
 void	print_split(char **tab);
