f7912571c63eea81837bbee648d1f335784311dc
mkulikov <mkulikov@student.42berlin.de>
2024-04-15 10:46:59 +0200

    refactor put and swap cmd

--- cmd_put.c
+++ cmd_put.c
@@ -6,22 +6,41 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 20:49:45 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/04/14 21:50:05 by mkulikov         ###   ########.fr       */
+/*   Updated: 2024/04/15 10:44:02 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	put_at_top(t_stack *stack1, t_stack *stack2)
+{
+	t_dlst	*head1_prev;
+	t_dlst	*head2_prev;
+
+	if (stack2->size == 0)
+		return ;
+	head1_prev = stack1->head;
+	head2_prev = stack2->head;
+	stack2->head = head2_prev->next;
+	stack2->head->prev = NULL;
+	stack1->head->prev = head2_prev;
+	stack1->head = head2_prev;
+	stack1->head->prev = NULL;
+	stack1->head->next = head1_prev;
+}
+
 void	pa(t_stack *a, t_stack *b)
 {
+	if (!b)
+		return;
 	write(1, "pa\n", 3);
-	if (a && b)
-		put_at_top(a, b);
+	put_at_top(a, b);
 }
 
 void	pb(t_stack *b, t_stack *a)
 {
+	if (!a)
+		return;
 	write(1, "pb\n", 3);
-	if (a && b)
-		put_at_top(b, a);
+	put_at_top(b, a);
 }
--- cmd_swap.c
+++ cmd_swap.c
@@ -6,28 +6,47 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 21:46:02 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/04/14 21:46:29 by mkulikov         ###   ########.fr       */
+/*   Updated: 2024/04/15 10:45:07 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	swap_first_2_elem(t_stack *stack)
+{
+	t_dlst	*temp;
+
+	if (stack->size <= 1)
+		return ;
+	temp = stack->head->next;
+	stack->head->prev = temp;
+	stack->head->next = temp->next;
+	stack->head->next->prev = stack->head;
+	temp->next = stack->head;
+	temp->prev = NULL;
+	stack->head = temp;
+}
+
 void	sa(t_stack *a)
 {
+	if (!a)
+		return ;
 	write(1, "sa\n", 3);
-	if (a)
-		swap_first_2_elem(a);
+	swap_first_2_elem(a);
 }
 
 void	sb(t_stack *b)
 {
+	if (!b)
+		return ;
 	write(1, "sb\n", 3);
-	if (b)
-		swap_first_2_elem(b);
+	swap_first_2_elem(b);
 }
 
 void	ss(t_stack *a, t_stack *b)
 {
+	if (!a && !b)
+		return ;
 	write(1, "ss\n", 3);
 	if (a)
 		swap_first_2_elem(a);
